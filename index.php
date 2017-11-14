abe54736f4e8c6e3d454bd34e7b8bf0cbedb65d1
pieter uitenbroek <p.uitenbroek@provenwebconcepts.nl>
2017-11-14 12:22:09 +0100

    Main structure and db connection

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>De Plantage</title>
+
+    <link rel="stylesheet" type="text/css" href="/assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <script src="/assets/js/jquery-3.2.1.min.js"></script>
+    <script src="/assets/js/main.js"></script>
+</head>
+<body>
+
+</body>
+</html>
