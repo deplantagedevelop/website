0a7ddf79dc076b9d7214fbedf289419156046c9c
pieter uitenbroek <p.uitenbroek@provenwebconcepts.nl>
2017-11-16 22:02:15 +0100

    removed error logging homepage

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
     include('header.php');
 
     $newsitems = $conn->query('SELECT * FROM news ORDER BY ID DESC LIMIT 3');
