90623888f3b20471eebc36fa5b1ce5e712831091
jeffreyhitv <s1114434@student.windesheim.nl>
2017-12-04 10:57:37 +0100

    Dashboard

--- dashboard.php
+++ dashboard.php
@@ -21,6 +21,8 @@ $countReviews = $conn->prepare("select count(*) from reviews");
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/assets/css/dashboard.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/slicknav.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/slick.css">
     <script src="https://use.fontawesome.com/be669cc78c.js"></script>
 </head>
 <body>
@@ -29,8 +31,23 @@ $countReviews = $conn->prepare("select count(*) from reviews");
         <div class="logo">
             <a href="/"><img src="/assets/images/dashlogo.png"></a>
         </div>
-        <div class="dashmenu">
-            <ul>
+        <div class="dashnav">
+            <div class="dropdown">
+                <button class="dropbtn"><i class="fa fa-bars" aria-hidden="true"></i></button>
+                <div class="dropdown-content">
+                    <a href="#">Home</a>
+                    <a href="#">Bestellingen</a>
+                    <a href="#">Accounts</a>
+                    <a href="#">Producten</a>
+                    <a href="#">Nieuws</a>
+                    <a href="#">Reviews</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Over ons</a>
+                    <a href="#">Product van de maand</a>
+                    <a href="#">Overige instelling</a>
+                </div>
+            </div>
+            <ul id="dashmenu">
                 <li><i class="fa fa-home" aria-hidden="true"></i><a href="/">&nbsp Home</a></li>
                 <li><i class="fa fa-shopping-basket" aria-hidden="true"></i><a href="/nieuws">&nbsp Bestellingen</a></li>
                 <li><i class="fa fa-users" aria-hidden="true"></i></i><a href="/shop">&nbsp Accounts</a></li>
@@ -39,11 +56,11 @@ $countReviews = $conn->prepare("select count(*) from reviews");
                 <li><i class="fa fa-star" aria-hidden="true"></i><a href="/contact">&nbsp Reviews</a></li>
                 <li><i class="fa fa-pencil-square-o" aria-hidden="true"></i><a href="/over-ons">&nbsp Contact</a></li>
                 <li><i class="fa fa-info-circle" aria-hidden="true"></i><a href="/reviews">&nbsp Over ons</a></li>
-                <li><i class="fa fa-coffee" aria-hidden="true"></i><a href="/contact">&nbsp Product van de maand</a>
-                </li>
+                <li><i class="fa fa-coffee" aria-hidden="true"></i><a href="/contact">&nbsp Product van de maand</a></li>
                 <li><i class="fa fa-cog" aria-hidden="true"></i><a href="/contact">&nbsp Overige instellingen</a></li>
             </ul>
         </div>
+        <div class="header-mobile"></div>
     </div>
     <div class="dashright">
         <div class="dashrightupper">
