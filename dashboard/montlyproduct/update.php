fb0f7eaa294f06a7f6c803a314374937e581165e
Ruben Van de Kamp <rubenvandekamp@149-104.dhcp.windesheim.nl>
2017-12-06 14:16:06 +0100

    little changes in monthlyproducts

--- dashboard/montlyproduct/update.php
+++ dashboard/montlyproduct/update.php
@@ -7,24 +7,53 @@
     $monthlyproduct->execute(array(
     ':id' => $id));
 
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $image = uniqid() . "-" . $_FILES['image']['name'];
+        $imagefile = $_FILES['image'];
+
+        if($imagefile['name'] == '') {
+            $stmt = $conn->prepare("UPDATE monthly_product SET title=:title, description=:description WHERE id=:id");
+        } else {
+            $stmt = $conn->prepare("UPDATE monthly_product SET title = :title, description = :description, image = :image WHERE id= :id ");
+            $stmt->bindparam(":image", $image);
+
+            $product = $conn->prepare("SELECT image FROM monthly_product WHERE ID = " . $id);
+            $product->execute();
+            $productimage = $product->fetch(PDO::FETCH_ASSOC);
+
+            $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/assets/images/monthlyproducts/";
+            $target_file = $target_dir . basename($image);
+            $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                return false;
+            } else {
+                unlink($_SERVER['DOCUMENT_ROOT'] . '/assets/images/monthlyproducts/' . $productimage['image']);
+                move_uploaded_file($imagefile["tmp_name"], $target_file);
+            }
+        }
+
+
+        $stmt->bindparam(":title", $_POST["title"]);
+        $stmt->bindparam(":description", $_POST["description"]);
+        $stmt->bindparam(":id", $id);
+        $stmt->execute();
+
+        $monthlyproduct = $conn->prepare("SELECT * FROM monthly_product WHERE ID= :id");
+        $monthlyproduct->execute(array(
+            ':id' => $id));
+    }
+
     while ($row = $monthlyproduct->fetch()) {
         $title = $row["title"];
         $description = $row["description"];
-        $image = $row["image"];
-    }
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $update_monthlyproduct = $conn->prepare("UPDATE monthly_product SET title=:title, description=:description WHERE id=:id");
-        $update_monthlyproduct->execute(array(
-            ':title' => $_POST["title"],
-            ':description' => $_POST["description"],
-            ':id' => $id
-        ));
+        $currentimage = $row["image"];
     }
 ?>
 
 <form class="update-monthlyproduct" method="post" enctype="multipart/form-data">
-    <img src="/assets/images/<?php echo $image ?>"> <br>
+    <img src="/assets/images/monthlyproducts/<?php echo $currentimage ?>"> <br>
     <input type="file" name="image" id="image"><br> <br>
     Titel: <br>
     <input type="text" name="title" value="<?php echo "$title";?>"> <br> <br>
@@ -33,6 +62,3 @@
     <input type="submit" name="submit" value="submit">
 </form>
 
-<?php
-    echo $image;
-?>
