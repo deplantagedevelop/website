6186d34d73bf316da041ffc196fd207caeb8e053
jeffreyhitv <s1114434@student.windesheim.nl>
2017-12-17 17:08:04 +0100

    orders

--- orders.php
+++ orders.php
@@ -5,10 +5,15 @@ include('header.php');
         $userid = $_SESSION['user_session'];
     }
 
-    $order = $conn->prepare("SELECT * FROM orders WHERE CustomerID = :id");
+    $order = $conn->prepare("SELECT o.ID, o.date, o.Status, amount*price as totaal, amount FROM orders as o JOIN orders as b      WHERE CustomerID = :id");
 
     $order->bindparam(":id", $userid);
     $order->execute();
+
+$producten = $conn->prepare("SELECT title, amount*price as totaal, amount FROM products P JOIN orderlines OL ON P.ID=OL.ProductID WHERE OL.OrderID=:id");
+
+$producten->bindparam(":id", $userid);
+$producten->execute();
 
 ?>
 <section class="content main-info">
@@ -28,11 +33,17 @@ include('header.php');
         <h1>Mijn bestellingen</h1>
 
         <?php
-            foreach ($order as $orders) {
-                echo $orders['ID'];
+            foreach ($order as $bestelling) {
+                ?>
+            <div class="fullorder">
+                <div class="ordercontent">
+                    <p><?php echo "Bestelnummer: <b>" . $bestelling['ID'] . "</b>" ?></p>
+                    <a><?php echo $bestelling['date']; ?></a>
+                </div>
+            </div>
+        <?php
             }
         ?>
-
     </div>
 
 
