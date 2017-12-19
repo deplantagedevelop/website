271491158cfdefa0ef9395c893b2ef8964ed882c
jeffreyhitv <s1114434@student.windesheim.nl>
2017-12-19 12:32:01 +0100

    orders

--- vieworder.php
+++ vieworder.php
@@ -3,13 +3,14 @@ include('header.php');
 
 $id = $_GET["id"];
 
-if(isset($_SESSION['user_session'])) {
-    if(isset($_GET['id'])) {
+if (isset($_SESSION['user_session'])) {
+    if (isset($_GET['id'])) {
         $userid = $_SESSION['user_session'];
         $id = $_GET['id'];
 
-        function datetime() {
-            return date( 'd-m-Y', time());
+        function datetime()
+        {
+            return date('d-m-Y', time());
         }
 
         $order = $conn->prepare("SELECT o.ID, o.date, o.Status, p.title , SUM(amount*price) as totaal, be.amount FROM orders as o JOIN orderlines as be ON o.ID = be.OrderID JOIN products as p ON be.ProductID = p.ID WHERE o.CustomerID = :userid AND o.ID = :id GROUP BY o.ID");
@@ -18,63 +19,71 @@ if(isset($_SESSION['user_session'])) {
         $order->execute();
         $order = $order->fetchAll();
 
-        $products = $conn->prepare("SELECT title, price, amount*price as totaal, amount FROM products P JOIN orderlines OL ON P.ID=OL.ProductID WHERE OL.OrderID=:id");
+        $products = $conn->prepare("SELECT title, image, price, amount*price as totaal, amount FROM products P JOIN orderlines OL ON P.ID=OL.ProductID WHERE OL.OrderID=:id");
         $products->execute(array(
             ":id" => $id
         ));
 
-            ?>
+        ?>
         <section class="content main-info">
-            <div class="allcontent">
+            <div class="fullcontent">
 
-            <div class="orderleft">
-                <h2>Mijn account</h2>
-                <ul>
-                    <li>Bestellingen</li>
-                    <li>Accountgegevens</li>
-                    <li>Uitloggen</li>
-                </ul>
-            </div>
+                <div class="leftcontent">
+                    <h2>Mijn account</h2>
+                    <ul>
+                        <li>Bestellingen</li>
+                        <li>Accountgegevens</li>
+                        <li>Uitloggen</li>
+                    </ul>
+                </div>
 
                 <?php
-                foreach ($order as $item) {
-                    ?>
-                    <div class="rightorder">
-                        <div class="breadcrumb">
-                            <a href="/orders">Bestellingen /</a>
-                            <h5><?php echo $id ?> </h5>
-                        </div>
+                foreach ($order
 
-                        <div class="orderinfo">
-                            <h2>Bestelnummer <?php echo $id ?></h2>
-
-
-
-
-
-
-
-                    <?php
-                }
+                as $item) {
                 ?>
+                <div class="rightorder">
+                    <div class="breadcrumb">
+                        <a href="/orders">Bestellingen /</a>
+                        <h5><?php echo $id ?> </h5>
+                    </div>
+                    <table>
+                        <tr>
+                                <th><b>Bestelnummer <?php echo $id ?></b></th>
+                                <th><b>Productnaam</b></th>
+                                <th><b>Aantal</b></th>
+                                <th><b>Prijs</b></th>
+                                <th><b>Totaal</b></th>
+                        </tr>
+                    <?php
+                    }
 
-            <?php
-            $totalprice = 0;
-        foreach ($products as $product) {
-            $title = $product["title"];
-            $price = $product["totaal"];
-            $priceeach = $product['price'];
-            $amount = $product["amount"];
-            $totalprice = $totalprice + $price;
-            echo "<h3> $title </h3> <a>Aantal &nbsp <b>$amount</b> </a><br><a>Prijs p.st. &nbsp <b>$priceeach</b></a><br><a>Totaal &nbsp <b>€$price</b> </a>";
-        }
-?>
-                <h3>Totaal &nbsp<?php echo $totalprice ?></h3>
+                    $totalprice = 0;
+                    foreach ($products as $product) {
+                    $title = $product["title"];
+                    $price = $product["totaal"];
+                    $priceeach = $product['price'];
+                    $amount = $product["amount"];
+                    $totalprice = $totalprice + $price;
+                    ?>
+                        <div class="tableheader">
+                            <tr>
+                                <td><img src="/assets/images/products/<?php echo $product["image"]; ?>"></td>
+                                <td><?php echo $title ?></td>
+                                <td><?php echo $amount ?></td>
+                                <td>€<?php echo $priceeach ?></td>
+                                <td>€<?php echo $price ?></td>
+                            </tr>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                    </table>
+                    <h3>Totaal &nbsp€<?php echo $totalprice ?></h3>
+                </div>
             </div>
-        </div>
 
         </section>
-
         <?php
     }
 } else {
